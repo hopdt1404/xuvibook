1c1a5c2ff7550127cc5604389f9039b75a2f0835
hopdt1404 <hopdt1404@gmail.com>
2020-12-13 10:43:30 +0700

    update

---
+++ xuvibook/resources/views/pages/not-found.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Xuvi Book</title>
+
+  <!-- Fonts -->
+
+
+  <!-- Styles -->
+
+</head>
+<body>
+<div id="app">
+  <h1>Quá trình xử lý gặp lỗi</h1>
+  <h3>Not found - 404</h3>
+</div>
+</body>
+<script src="{{ mix('/js/app.js') }}"></script>
+</html>
